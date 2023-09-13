755b09f3a561a156b47c8f80dba571af7f4ce774
devi5040 <dpraidola@gmail.com>
2023-09-13 21:56:08 +0530

    The solution with best time complexity

--- SubArrayWithGivenSum/bestSolution.c
+++ SubArrayWithGivenSum/bestSolution.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int n, sum, i, j, cur, arr[100];
+    int n, sum, i, j, s = 0, arr[100], cur = 0;
     printf("Enter the array size:\n");
     scanf("%d", &n);
     printf("Enter the array Elements:\n");
@@ -11,14 +11,15 @@ int main()
     scanf("%d", &sum);
     for (i = 0; i < n; i++)
     {
-        cur = 0;
-        for (j = i; j < n; j++)
+        cur += arr[i];
+        while (sum < cur)
         {
-            cur += arr[j];
-            if (cur > sum)
-                break;
-            if (sum == cur)
-                printf("The sub arrays starts from %d to %d", i, j);
+            cur -= arr[s];
+            s++;
+        }
+        if (cur == sum)
+        {
+            printf("%d %d", i, s);
         }
     }
 }
